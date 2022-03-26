a9fcae2f98f25f1dae48176e7cdcce64faea3e92
Tinghui <tinghui1738cheng@gmail.com>
2022-03-26 10:07:50 +0800

    HW01

---
+++ ai2022s-hw1 /HW1_search/search.py
@@ -0,0 +1,236 @@
+# search.py
+# ---------
+# Licensing Information:  You are free to use or extend these projects for
+# educational purposes provided that (1) you do not distribute or publish
+# solutions, (2) you retain this notice, and (3) you provide clear
+# attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
+# 
+# Attribution Information: The Pacman AI projects were developed at UC Berkeley.
+# The core projects and autograders were primarily created by John DeNero
+# (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
+# Student side autograding was added by Brad Miller, Nick Hay, and
+# Pieter Abbeel (pabbeel@cs.berkeley.edu).
+
+
+"""
+In search.py, you will implement generic search algorithms which are called by
+Pacman agents (in searchAgents.py).
+"""
+
+import util
+
+class SearchProblem:
+    """
+    This class outlines the structure of a search problem, but doesn't implement
+    any of the methods (in object-oriented terminology: an abstract class).
+
+    You do not need to change anything in this class, ever.
+    """
+
+    def getStartState(self):
+        """
+        Returns the start state for the search problem.
+        """
+        util.raiseNotDefined()
+
+    def isGoalState(self, state):
+        """
+          state: Search state
+
+        Returns True if and only if the state is a valid goal state.
+        """
+        util.raiseNotDefined()
+
+    def getSuccessors(self, state):
+        """
+          state: Search state
+
+        For a given state, this should return a list of triples, (successor,
+        action, stepCost), where 'successor' is a successor to the current
+        state, 'action' is the action required to get there, and 'stepCost' is
+        the incremental cost of expanding to that successor.
+        """
+        util.raiseNotDefined()
+
+    def getCostOfActions(self, actions):
+        """
+         actions: A list of actions to take
+
+        This method returns the total cost of a particular sequence of actions.
+        The sequence must be composed of legal moves.
+        """
+        util.raiseNotDefined()
+
+
+def tinyMazeSearch(problem):
+    """
+    Returns a sequence of moves that solves tinyMaze.  For any other maze, the
+    sequence of moves will be incorrect, so only use this for tinyMaze.
+    """
+    from game import Directions
+    s = Directions.SOUTH
+    w = Directions.WEST
+    return  [s, s, w, s, w, w, s, w]
+
+def depthFirstSearch(problem):
+    """
+    Search the deepest nodes in the search tree first.
+
+    Your search algorithm needs to return a list of actions that reaches the
+    goal. Make sure to implement a graph search algorithm.
+
+    To get started, you might want to try some of these simple commands to
+    understand the search problem that is being passed in:
+
+    print "Start:", problem.getStartState()
+    print "Is the start a goal?", problem.isGoalState(problem.getStartState())
+    print "Start's successors:", problem.getSuccessors(problem.getStartState())
+    """
+    "*** YOUR CODE HERE ***"
+    
+    # Command:
+    # /usr/local/bin/python pacman.py -l tinyMaze -p SearchAgent -a fn=dfs
+    # /usr/local/bin/python pacman.py -l mediumMaze -p SearchAgent -a fn=dfs
+    # /usr/local/bin/python pacman.py -l bigMaze -z .5 -p SearchAgent -a fn=dfs
+    # /usr/local/bin/python autograder.py -q q1
+    
+    
+    #Set up the stack and visiting checklist for DFS and store start state into the stack
+    Start = problem.getStartState()
+    Store = util.Stack()
+    Visited = []
+    Store.push((Start,[]))
+    #print(Start)
+    
+    #Repeat the seacrching if there is a item inside the stack and still not find the goal
+    while not Store.isEmpty():
+        #Get the last state and its moving action from the stack 
+        State, Moving = Store.pop()
+        
+        #Check if the state isn't visited  
+        if State not in Visited:
+            Visited.append(State)
+            #If the state is goal, return the moving. Otherwise, to get the next node 
+            if problem.isGoalState(State):
+                return Moving
+            else:
+                for successor, action, stepCost in problem.getSuccessors(State):
+                    Store.push((successor, Moving+[action]))
+        
+    util.raiseNotDefined()
+
+def breadthFirstSearch(problem):
+    """Search the shallowest nodes in the search tree first."""
+    "*** YOUR CODE HERE ***"
+    
+    # Command:
+    # /usr/local/bin/python pacman.py -l tinyMaze -p SearchAgent -a fn=bfs
+    # /usr/local/bin/python pacman.py -l mediumMaze -p SearchAgent -a fn=bfs
+    # /usr/local/bin/python pacman.py -l bigMaze -z .5 -p SearchAgent -a fn=bfs
+    # /usr/local/bin/python autograder.py -q q2
+    
+    #Set up the queue and visiting checklist for BFS and store start state into the queue
+    Start = problem.getStartState()
+    Store = util.Queue()
+    Visited = []
+    Store.push((Start,[]))
+    
+    
+    #Repeat the seacrching if there is a item inside the queue and still not find the goal
+    while not Store.isEmpty():
+        #Get the last state and its moving action from the queue 
+        State, Moving = Store.pop()
+       
+        #Check if the state isn't visited  
+        if State not in Visited:
+            Visited.append(State)
+            #If the state is goal, return the moving. Otherwise, to get the next node 
+            if problem.isGoalState(State):
+                return Moving
+            else:
+                for successor, action, stepCost in problem.getSuccessors(State):
+                    Store.push((successor, Moving+[action]))
+                    #print(successor, action, stepCost)
+                    
+    
+    util.raiseNotDefined()
+
+def uniformCostSearch(problem):
+    """Search the node of least total cost first."""
+    "*** YOUR CODE HERE ***"
+    # Command:
+    # /usr/local/bin/python pacman.py -l mediumMaze -p SearchAgent -a fn=bfs
+    # /usr/local/bin/python pacman.py -l mediumMaze -p SearchAgent -a fn=ucs
+    # /usr/local/bin/python pacman.py -l mediumDottedMaze -p StayEastSearchAgent
+    # /usr/local/bin/python pacman.py -l mediumScaryMaze -p StayWestSearchAgent
+    # /usr/local/bin/python autograder.py -q q3
+    
+    #Set up the queue, visiting checklist, actions for UCS 
+    Start = problem.getStartState()
+    Store = util.PriorityQueue()
+    Visited = []
+    
+    #Store the starting state, action, cost and the priority into the priority queue
+    Store.push((Start, []),0)
+    
+    #Repeat the seacrching if there is a item inside the priorty queue and still not find the goal
+    while not Store.isEmpty():
+        #Get the last state and its moving action from the priorty queue 
+        State, Moving = Store.pop()
+        
+        #Check if the state isn't visited  
+        if State not in Visited:
+            Visited.append(State)
+            #If the state is goal, return the moving. Otherwise, to get the next node 
+            if problem.isGoalState(State):
+                return Moving
+            else:
+                for successor, action, stepCost in problem.getSuccessors(State):
+                    Store.update((successor, Moving+[action]), problem.getCostOfActions(Moving+[action]))
+                    #print((successor, Moving+[action], Allcost))
+    util.raiseNotDefined()
+
+def nullHeuristic(state, problem=None):
+    """
+    A heuristic function estimates the cost from the current state to the nearest
+    goal in the provided SearchProblem.  This heuristic is trivial.
+    """
+    return 0
+
+def aStarSearch(problem, heuristic=nullHeuristic):
+    """Search the node that has the lowest combined cost and heuristic first."""
+    "*** YOUR CODE HERE ***"
+    #command: 
+    # /usr/local/bin/python pacman.py -l bigMaze -z .5 -p SearchAgent -a fn=astar,heuristic=manhattanHeuristic
+    # /usr/local/bin/python autograder.py -q q3
+    #Set up the queue, visiting checklist, actions for UCS 
+    Start = problem.getStartState()
+    Store = util.PriorityQueue()
+    Visited = []
+    
+    #Store the starting state, action, cost and the priority into the priority queue
+    Store.push((Start, []),heuristic(Start, problem))
+    
+    #Repeat the seacrching if there is a item inside the priorty queue and still not find the goal
+    while not Store.isEmpty():
+        #Get the last state and its moving action from the priorty queue 
+        State, Moving = Store.pop()
+        
+        #Check if the state isn't visited  
+        if State not in Visited:
+            Visited.append(State)
+            #If the state is goal, return the moving. Otherwise, to get the next node 
+            if problem.isGoalState(State):
+                return Moving
+            else:
+                for successor, action, stepCost in problem.getSuccessors(State):
+                    Store.update((successor, Moving+[action]), problem.getCostOfActions(Moving+[action]) + heuristic(successor, problem))
+                    #print((successor, Moving+[action], Allcost))
+    util.raiseNotDefined()
+
+
+# Abbreviations
+bfs = breadthFirstSearch
+dfs = depthFirstSearch
+astar = aStarSearch
+ucs = uniformCostSearch
